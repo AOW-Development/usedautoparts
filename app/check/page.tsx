919f2264f8934becebedbde40863bbb25acd0395
ChinmayaAOW <ChinmayaSamal@aow.co.in>
2025-12-05 03:38:29 +0000

    modifications

---
+++ app/check/page.tsx
@@ -0,0 +1,12 @@
+import AboutSection from '@/components/home/AboutSection'
+import React from 'react'
+
+function page() {
+  return (
+    <div className='h-full'>
+      <AboutSection/>
+    </div>
+  )
+}
+
+export default page
